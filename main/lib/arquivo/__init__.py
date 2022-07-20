69be78325f012db0a6d40c33a1652626ccbae096
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-20 13:13:40 -0300

    Update __init__.py

--- main/lib/arquivo/__init__.py
+++ main/lib/arquivo/__init__.py
@@ -1,10 +1,9 @@
 from time import sleep
 
 
-def arquivoExiste(nomeArquivo):
-    with open(nomeArquivo, 'wt+'):
-        pass
-
-
-def cadastrar(nome, idade):
-    pass
+def cadastrar(nomeArquivo, nome, idade):
+    with open(nomeArquivo, 'at+') as arquivo:
+        arquivo.write(f'{nome};{idade}\n')
+    sleep(1)
+    print(f'\nCadastro realizado com sucesso!'.center(35))
+    sleep(1)
