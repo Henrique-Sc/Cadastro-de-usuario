3e7fa5555f377c0d34a6dd98465fd66cb3c2e499
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-20 11:18:09 -0300

    Atualizar main.py

--- main/main.py
+++ main/main.py
@@ -1,19 +1,23 @@
 from lib.interface import *
+from lib.arquivo import *
 
 while True:
     titulo('CADASTRO DE USUÁRIO', corSimb=4)
     esc = opcoes('Cadastrar', 'Lista dos cadastros', 'Sair do programa', corNum=1, corLinha=3)
 
+    # Cadastrar
     if esc == 1:
         titulo('Cadastrar', corSimb=3)
-        print('aa')
+
         sleep(1)
 
+    # Listar os cadastros
     elif esc == 2:
         titulo('Lista dos cadastros', corSimb=3)
-        print('aa')
+
         sleep(1)
 
+    # Sair do programa
     elif esc == 3:
         titulo('Saindo do programa...', corSimb=3)
         print(f'{cores[4]}{"-> Volte sempre! <-":^30}{cores[0]}')
