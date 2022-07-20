d597a7231c208ba7a87fb9c4b2775a12627efe7b
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-20 13:31:47 -0300

    Update __init__.py

--- main/lib/arquivo/__init__.py
+++ main/lib/arquivo/__init__.py
@@ -7,3 +7,12 @@ def cadastrar(nomeArquivo, nome, idade):
     sleep(1)
     print(f'\nCadastro realizado com sucesso!'.center(35))
     sleep(1)
+
+
+def mostrarCadastro(nomeArquivo, tmn=35):
+    with open(nomeArquivo, 'rt+') as arquivo:
+        for cadastro in arquivo:
+            cadastro = cadastro.replace('\n', '').split(';')
+            print(f' {cadastro[0]:<{tmn-11}} {cadastro[1]:<3}', 'ano' if int(cadastro[1]) <= 1 else 'anos')
+            sleep(0.23)
+    sleep(0.90)
