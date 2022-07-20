7dd054da7f4697432080376afb3f57dbc66dd13d
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-20 14:25:41 -0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -4,25 +4,29 @@ from lib.dado import *
 
 nomeArquivo = 'cadastros.txt'
 while True:
-    titulo('CADASTRO DE USUÁRIO', corSimb=4)
-    esc = opcoes('Cadastrar', 'Lista dos cadastros', 'Sair do programa', corNum=1, corLinha=3)
+    try:
+        titulo('CADASTRO DE USUÁRIO', corSimb=4)
+        esc = opcoes('Cadastrar', 'Lista dos cadastros', 'Sair do programa', corNum=1, corLinha=3)
 
-    # Cadastrar
-    if esc == 1:
-        titulo('Cadastrar', corSimb=3)
+        # Cadastrar
+        if esc == 1:
+            titulo('Cadastrar', corSimb=3)
 
-        nome = leiaNome('Digite o seu nome: ')
-        idade = leiaIdade('Digite a sua idade: ')
-        cadastrar(nomeArquivo, nome, idade)
+            nome = leiaNome('Digite o seu nome: ')
+            idade = leiaIdade('Digite a sua idade: ')
+            cadastrar(nomeArquivo, nome, idade)
 
-    # Listar os cadastros
-    elif esc == 2:
-        titulo('Lista dos cadastros', corSimb=3)
-        mostrarCadastro(nomeArquivo, tmn=35)
+        # Listar os cadastros
+        elif esc == 2:
+            titulo('Lista dos cadastros', corSimb=3)
+            mostrarCadastro(nomeArquivo, tmn=35)
 
-    # Sair do programa
-    elif esc == 3:
-        titulo('Saindo do programa...', corSimb=3)
-        print(f'{cores[4]}{"-> Volte sempre! <-":^30}{cores[0]}')
-        sleep(1)
-        break
+        # Sair do programa
+        elif esc == 3:
+            titulo('Saindo do programa...', corSimb=3)
+            print(f'{cores[4]}{"-> Volte sempre! <-":^30}{cores[0]}')
+            sleep(1)
+            break
+
+    except KeyboardInterrupt:
+        msgErro('ERRO: O usuário interrompeu o programa.')
