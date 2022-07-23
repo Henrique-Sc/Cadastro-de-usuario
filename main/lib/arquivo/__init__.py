0824969a55c78ba2501d887c1d1ee776230da246
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-23 11:30:41 -0300

    Update __init__.py

--- main/lib/arquivo/__init__.py
+++ main/lib/arquivo/__init__.py
@@ -1,8 +1,21 @@
 from time import sleep
 
 
+def arquivoExiste(nomeArquivo):
+    try:
+        with open(nomeArquivo, 'r'):
+            pass
+
+    except FileNotFoundError:
+        with open(nomeArquivo, 'w+'):
+            pass
+
+    except:
+        print('\033[37mErro ao criar o arquivo.')
+
+
 def cadastrar(nomeArquivo, nome, idade):
-    with open(nomeArquivo, 'at+', encoding='utf-8') as arquivo:
+    with open(nomeArquivo, 'a', encoding='utf-8') as arquivo:
         arquivo.write(f'{nome};{idade}\n')
     sleep(1)
     print(f'\nCadastro realizado com sucesso!'.center(35))
@@ -10,9 +23,14 @@ def cadastrar(nomeArquivo, nome, idade):
 
 
 def mostrarCadastro(nomeArquivo, tmn=35):
-    with open(nomeArquivo, 'rt+', encoding='utf-8') as arquivo:
-        for cadastro in arquivo:
-            cadastro = cadastro.replace('\n', '').split(';')
-            print(f' {cadastro[0]:<{tmn-11}} {cadastro[1]:<3}', 'ano' if int(cadastro[1]) <= 1 else 'anos')
-            sleep(0.23)
-    sleep(0.90)
+    with open(nomeArquivo, 'r', encoding='utf-8') as arquivo:
+        listaCadastro = arquivo.readlines()
+
+    if not listaCadastro:
+        print('Nenhum cadastro encontrado.\n'.center(tmn))
+
+    for cadastro in listaCadastro:
+        cadastro = cadastro.replace('\n', '').split(';')
+        print(f' {cadastro[0]:<{tmn - 11}} {cadastro[1]:<3}', 'ano' if int(cadastro[1]) <= 1 else 'anos')
+        sleep(0.23)
+    sleep(1)
